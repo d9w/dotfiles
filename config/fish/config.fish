cd79dcec5015655352eb7fc0795b23d3f42a3005
Dennis Wilson <d9w@mit.edu>
2016-04-02 22:22:21 +0200

    fish git submodule pulleach

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,6 +3,16 @@ function fish_greeting
     fortune
 end
 
+function git -d 'Some added git functions'
+    if test (count $argv) -eq 2
+        if test $argv[1] = 'submodule' -a $argv[2] = 'pulleach'
+            command git submodule foreach git pull origin master
+            return 0
+        end
+    end
+    command git $argv
+end
+
 # colored man pages
 set -x LESS_TERMCAP_mb (printf "\e[01;31m")
 set -x LESS_TERMCAP_md (printf "\e[01;31m")
@@ -14,6 +24,8 @@ set -x LESS_TERMCAP_us (printf "\e[01;32m")
 
 # Disable virtual env prompt
 set -x VIRTUAL_ENV_DISABLE_PROMPT True
+
+eval (keychain --eval --quiet id_rsa)
 
 # prompts
 . ~/.config/fish/helpers.fish
